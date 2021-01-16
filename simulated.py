6a75f48f7aed3dc0ac6112e79b397d75c5bdd1d1
Smidra <rad.smid@gmail.com>
2021-01-16 12:26:32 +0100

    Flip shourl work.

--- simulated.py
+++ simulated.py
@@ -65,6 +65,13 @@ class CNFState:
 
     # Change one bit in state and refresh values
     def flip(self, item_nr):
+        if self.truth_values_array[item_nr] == 1:
+            self.truth_values_array[item_nr] == 0
+        elif self.truth_values_array[item_nr] == 0:
+            self.truth_values_array[item_nr] == 1
+        else:
+            print("Unexpected value in truth values array.")
+        self.refresh()
         return False
 
     # Randomizes the state completely
